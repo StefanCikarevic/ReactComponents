8bdacd11dbc1ca22e494243464f20eaf3a3fd0a9
StefanCikarevic <stefancikarevic1996@gmail.com>
2022-05-04 22:20:38 +0200

    test commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
-
 function App() {
   const expenses = [
     {
@@ -28,6 +27,7 @@ function App() {
   const addExpanseHandler = (expense) => {
     console.log("APP");
     console.log(expense);
+    console.log("stefke");
   };
 
   return (
